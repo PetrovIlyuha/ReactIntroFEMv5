0f362134022b27ac09e03911c83e9a1e36afa6b2
Ilya Petrov <dnamix1@gmail.com>
2019-10-23 13:30:32 +0300

    created generic dropdown menu

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
+  const [animal, updateAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const [breeds, setBreeds] = useState([]);
 
   return (
     <div className="search-params">
@@ -15,6 +19,38 @@ const SearchParams = () => {
             onChange={e => setLocation(e.target.value)}
           />
           <button>Submit</button>
+        </label>
+        <label htmlFor="animal">
+          animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={e => updateAnimal(e.target.value)}
+            onBlur={e => updateAnimal(e.target.value)}
+          >
+            {ANIMALS.map(animal => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            disabled={!breed.length}
+            value={breed}
+            onChange={e => setBreed(e.target.value)}
+            onBlur={e => setBreed(e.target.value)}
+          >
+            <option>All</option>
+            {breeds.map(breedString => {
+              <option value={breedString} key={breedString}>
+                {breedString}
+              </option>;
+            })}
+          </select>
         </label>
       </form>
     </div>
