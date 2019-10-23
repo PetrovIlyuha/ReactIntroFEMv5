a609c5bc37d325fb2fe480e319e7389a64e97332
Ilya Petrov <dnamix1@gmail.com>
2019-10-23 12:48:48 +0300

    added useState hook for Search Field

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
-
+import SearchParams from "./SearchParams";
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Dry" animal="Fish" breed="party" />
-      <Pet name="Smoke" animal="hawk" breed="Rocky Hawk" />
-      <Pet name="Smiley" animal="tortoise" breed="sea turtle" />
+      <SearchParams />
     </div>
   );
 };
